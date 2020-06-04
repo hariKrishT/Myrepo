183b1cf9ea639279096ad69a05ec146f6fb623c3
HONEY <harikrishnatippa@gmail.com>
2020-06-04 18:28:01 +0100

    new test project

---
+++ Test/src/tst/Welcome.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package tst;
+
+/**
+ * @author HONEY
+ *
+ */
+public class Welcome {
+
+	/**
+	 * 
+	 */
+	public Welcome() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Welcome");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Java");
+
+	}
+
+}
